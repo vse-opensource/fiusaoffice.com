722c82909f4d25ad22901bc56837ec1b27babb2c
vse-opensource <virtualspace.suporte@gmail.com>
2023-02-13 02:52:30 -0300

    update

--- app.js
+++ app.js
@@ -13,3 +13,18 @@ function navMenu2(){ closeMenu(); scrollTo("#sobre"); }
 function navMenu3(){ closeMenu(); scrollTo("#servicos"); }
 function navMenu4(){ closeMenu(); scrollTo("#endereco"); }
 function agendar(){  closeMenu(); scrollTo("#agendar"); }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
---
+++ vse/engine.js
@@ -0,0 +1,28 @@
+// w3-include modified (https://www.w3schools.com/howto/howto_html_include.asp)
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("add");
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                    if (this.status == 404) {elmnt.innerHTML = "Arquivo não encontrado";}
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute("add");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /* Exit the function: */
+            return;
+        }
+    }
+}
